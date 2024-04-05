773cf496d883b8497344ccfafe7533067f2cbd70
jajangmyeonjoa <jajangmyeonjoa@gmail.com>
2024-04-05 15:39:51 +0900

    bot3

--- bot3.js
+++ bot3.js
@@ -4,8 +4,8 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
 	if (msg === "!") {
 		replier.reply("Hello, World!");
 	} else if (msg == '!홍성범') {
-        	replier.reply("간지~");
+        	replier.reply("간지");
 	} else if (msg == '!홍기석') {
-        replier.reply("대가리 몽키새끼~");
+        replier.reply("대가리 몽키~");
     }
 }
